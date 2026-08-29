ef08e36161fd316d44a99913c6caec4fd5566167
Shankar G L <shankarshankargl2007@gmail.com>
2026-08-29 19:43:01 +0530

    Time: 2 ms (88.95%) | Memory: 20.6 MB (46.24%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,24 +1,26 @@
 class Solution:
-    def findCircleNum(self, num: List[List[int]]) -> int:
-        def dfs(node,add_arr,visited):
-            visited[node]=True
-            for j in add_arr[node]:
-                if not visited[j]:
-                    dfs(j,add_arr,visited)
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        m=len(isConnected)
+        n=len(isConnected[0])
+        def dfs(node,visited,add_list):
+            visited[node]=1
+            for nei in add_list[node]:
+                if visited[nei]==0:
+                    dfs(nei,visited,add_list)
+
+        graph=[[] for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if isConnected[i][j]==1 and i!=j:
+                    graph[i].append(j)
+                    graph[j].append(i)
         
-        n=len(num)
-        m=len(num[0])
-        add_arr=[[] for _ in range(n)]
-        for i in range(n):
-            for j in range(m):
-                if num[i][j]==1 and i!=j:
-                    add_arr[i].append(j)
-                    add_arr[j].append(i)
-        visited=[False]*n
+        visited=[0]*m
         count=0
-        for i in range(n):
-            if not visited[i]:
+        for i in range(m):
+            if visited[i]==0:
                 count+=1
-                dfs(i,add_arr,visited)
+                dfs(i,visited,graph)
         return count
+        
         

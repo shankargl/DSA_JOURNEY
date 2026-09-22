24279ebe709e624bd98ad8f424ca5b8641920c93
Shankar G L <shankarshankargl2007@gmail.com>
2026-09-22 18:40:29 +0530

    Time: 5 ms (14.46%) | Memory: 30 MB (5.43%) - LeetSync

---
+++ 344-reverse-string/reverse-string.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reverseString(self, s: list[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        def reverse(l,r):
+            if l>r:
+                return s
+            s[l],s[r]=s[r],s[l]
+            return reverse(l+1,r-1)
+        return reverse(0,len(s)-1)
